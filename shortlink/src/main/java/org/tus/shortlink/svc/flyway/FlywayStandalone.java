a0b7a9938421f6c5d5beabd16e616d98bba76ab2
Rurutia1027 <rurutia1027@gmail.com>
2026-01-15 16:24:38 +0800

    refactor migratio name and add main entrypoing for executing data migration via flyway

---
+++ shortlink/src/main/java/org/tus/shortlink/svc/flyway/FlywayStandalone.java
@@ -0,0 +1,27 @@
+package org.tus.shortlink.svc.flyway;
+
+import org.flywaydb.core.Flyway;
+
+public class FlywayStandalone {
+
+    public static void main(String[] args) {
+
+        // Configure Flyway
+        Flyway flyway = Flyway.configure()
+                .dataSource(
+                        "jdbc:postgresql://localhost:5432/shortlink",
+                        "admin",
+                        "admin")
+                .table("flyway_schema_history")
+                .locations("classpath:db/migration")      // SQL migrations
+                // "classpath:flyway-java-migrations") // Java-based migrations
+                .baselineOnMigrate(false)
+                .outOfOrder(true)
+                .load();
+
+        flyway.migrate();
+
+        System.out.println("Flyway migrations completed successfully.");
+        System.exit(0);
+    }
+}
