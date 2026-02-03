c32d09e0de10f3279a64a524ef1158c8a0221426
Rurutia1027 <rurutia1027@gmail.com>
2026-02-03 20:42:50 +0800

    add gateway impl for shortlink, this GW focusing on handling app layer gateway request, compatible with istio GW(arch traffic oriented)

---
+++ gateway/src/main/java/org/tus/shortlink/gateway/config/GatewayConfig.java
@@ -0,0 +1,39 @@
+package org.tus.shortlink.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+/**
+ * Gateway configuration
+ */
+@Configuration
+public class GatewayConfig {
+
+    /**
+     * Rate limiter key resolver - use IP address as key
+     * Can be customized to use user ID, API key, etc.
+     */
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> {
+            String ip = exchange.getRequest().getRemoteAddress() != null
+                    ? exchange.getRequest().getRemoteAddress().getAddress().getHostAddress()
+                    : "unknown";
+            return Mono.just(ip);
+        };
+    }
+
+    /**
+     * User-based key resolver (if authentication is enabled)
+     * Uncomment and configure when user authentication is implemented
+     */
+    // @Bean
+    // public KeyResolver userKeyResolver() {
+    //     return exchange -> {
+    //         String userId = exchange.getRequest().getHeaders().getFirst("X-User-Id");
+    //         return Mono.just(userId != null ? userId : "anonymous");
+    //     };
+    // }
+}
